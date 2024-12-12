90c1f323aebce7d89c4739a5ca3670cccc4b0c3b
keelus <hugomoreda@hotmail.com>
2024-12-12 19:33:30 +0100

    Finalizar.

--- hospital.c
+++ hospital.c
@@ -11,7 +11,8 @@
 #include <mqueue.h>
 #include <string.h>
 
-#define NOMBRE_PACIENTE_LEN 128
+// Define el tamaño de los nombres de pacientes
+#define NOMBRE_PACIENTE_LEN 32
 
 int pacientes_dados_de_alta = 0;
 pid_t pid_hospital, pid_recepcion;
@@ -35,8 +36,6 @@ int tiempo_aleatorio(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
 
-char* generar_nombre_paciente();
-
 void* exploracion(void* args) {
 	printf(" [Exploración] Comienzo mi ejecución...\n");
 
@@ -45,12 +44,15 @@ void* exploracion(void* args) {
 		char paciente[NOMBRE_PACIENTE_LEN];
 		printf(" [Exploración] Esperando a un paciente...\n");
 
+		// Se bloqueará hasta recibir un nombre
+		// de paciente desde Recepción.
 		resultado = mq_receive(cola_recep_explor, paciente, sizeof(paciente), 0);
 
 		if(resultado == -1) {
 			fprintf(stderr, " [ERROR][Exploración] Error al leer de la cola. (Codigo: %d)\n", errno);
 			exit(1);
 		} else {
+			// Realizar la exploración
 			printf(" [Exploración] Se ha recibido el paciente \"%s\"\n", paciente);
 
 			printf(" [Exploración] Recibido paciente: %s. Realizando exploración...\n", paciente);
@@ -66,32 +68,70 @@ void* exploracion(void* args) {
 void* diagnostico(void* args) {
     printf("  [Diagnóstico] Comienzo mi ejecución...\n");
     while (1) {
+		// Bloquearse hasta que haya pacientes que diagnosticar.
 		sem_wait(sem_diagnostico);
+
         printf("  [Diagnóstico] Realizando pruebas diagnósticas...\n");
         sleep(tiempo_aleatorio(5, 10));
         printf("  [Diagnóstico] Diagnóstico completado. Notificando farmacia...\n");
+
+		// Mandar el paciente a la farmacia.
 		sem_post(sem_farmacia);
     }
 }
 
 void* farmacia(void* args) {
-	int pid_recepcion = *((int *)args);
     printf("   [Farmacia] Comienzo mi ejecución...\n");
     while (1) {
+		// Bloquearse hasta que haya pacientes que diagnosticar.
 		sem_wait(sem_farmacia);
+
         printf("   [Farmacia] Preparando medicación...\n");
         sleep(tiempo_aleatorio(1, 3));
         printf("   [Farmacia] Medicación lista. Enviando señal de alta...\n");
+
+		// Notificar a Recepción de que el paciente debe
+		// de ser dado de alta.
 		kill(pid_recepcion, SENYAL_FARMACIA_RECEPCION);
     }
 }
 
+#define NUM_NOMBRES 50
+// Función para obtener un nombre de paciente aleatoriamente,
+// para dar algo más de información a los primeros prints.
+char* generar_nombre_paciente() {
+	// Nota: Este array de nombres ha sido
+	// generada con ChatGPT.
+	// https://chatgpt.com/share/6759e9f5-8740-800e-a3a8-467ffc634fac
+
+	static char nombres[NUM_NOMBRES][NOMBRE_PACIENTE_LEN] = {
+    	"Alejandro", "Beatriz", "Carlos", "Diana", "Eduardo",
+    	"Fernanda", "Gabriel", "Héctor", "Isabel", "Javier",
+    	"Karla", "Luis", "María", "Natalia", "Oscar",
+    	"Patricia", "Quirino", "Rafael", "Sofía", "Tomás",
+    	"Úrsula", "Vicente", "Walter", "Ximena", "Yolanda",
+    	"Zaira", "Andrea", "Braulio", "Camila", "Daniel",
+    	"Esteban", "Francisca", "Guillermo", "Hilda", "Iván",
+    	"José", "Laura", "Martín", "Nicolás", "Olga",
+    	"Paola", "Ramón", "Salvador", "Teresa", "Ulises",
+    	"Valeria", "Wilfredo", "Xiomara", "Yahir", "Zoila"
+	};
+
+	return nombres[rand() % NUM_NOMBRES];
+}
+
+// Contabiliza la salida del paciente, aumentando
+// el contador de pacientes dados de alta en 1.
+// Tambien, llama a signal para continuar a la escucha
+// de la señal proveniente de Farmacia.
 void contabilizar_salida(int sig) {
 	pacientes_dados_de_alta++;
 	printf("[Recepción] <- Un paciente sale... %d dados de alta en total.\n", pacientes_dados_de_alta);
 	signal(SENYAL_FARMACIA_RECEPCION, contabilizar_salida);
 }
 
+// Simplemente sale del programa. Es
+// usada por el hilo Recepción y Hospital.
 void terminar_hilo(int sig) {
 	exit(0);
 }
@@ -107,11 +147,11 @@ int main(int argv, char* argc[]) {
 	sem_unlink(SEM_DIAGNOSTICO);
 	sem_unlink(SEM_FARMACIA);
 
-	// Crear cola
+	// Crear cola para "Recepción" -> "Exploración"
 	struct mq_attr attr;
 	attr.mq_flags = 0;
-	attr.mq_maxmsg= 1;
-	attr.mq_msgsize = NOMBRE_PACIENTE_LEN;
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize = 32;
 	attr.mq_curmsgs = 0;
 
 	cola_recep_explor = mq_open(COLA_RECEP_EXPLOR, O_RDWR | O_CREAT, 0644, &attr);
@@ -128,7 +168,7 @@ int main(int argv, char* argc[]) {
 		exit(1);
 	}
 
-	// Crear semaforo para diagnostico
+	// Crear semaforo para farmacia
 	sem_farmacia = sem_open(SEM_FARMACIA, O_CREAT, 0644, 0);
 	if(sem_farmacia == SEM_FAILED) {
 		perror("Error al crear el semaforo para farmacia.");
@@ -141,8 +181,7 @@ int main(int argv, char* argc[]) {
 		pid_hospital = fork();
 		if (pid_hospital != 0) {
 			printf("[PADRE (%d)] A la espera del fin de hijos.\n", getpid());
-
-			printf("No hacer nada!\n");
+			// Evitar que el padre reaccione a CTRL+C.
 			signal(SIGINT, SIG_IGN);
 
 			// Proceso padre
@@ -150,6 +189,7 @@ int main(int argv, char* argc[]) {
 			fin_recepcion = 0;
 			fin_hospital = 0;
 
+			// Esperar a la terminación de ambos hijos.
 			while(!fin_recepcion || !fin_hospital) {
 				pid_recibida = wait(&estado);
 
@@ -182,7 +222,7 @@ int main(int argv, char* argc[]) {
 			int i;
 			for(i = 0; i < 3; i++) {
 				pthread_attr_init(&attr_hilos[i]);
-				pthread_create(&id_hilos[i], &attr_hilos[i], func_hilos[i], (void *)&pid_recepcion);
+				pthread_create(&id_hilos[i], &attr_hilos[i], func_hilos[i], NULL);
 			}
 
 			for(i = 0; i < 3; i++) pthread_join(id_hilos[i], NULL);
@@ -190,9 +230,13 @@ int main(int argv, char* argc[]) {
 	} else {
 		// Proceso recepción
 		printf("[Recepción] Comienzo mi ejecución...\n");
+		// Configurar señales de terminacion por SIGINT y
+		// contabilizar salida.
 		signal(SIGINT, terminar_hilo);
 		signal(SENYAL_FARMACIA_RECEPCION, contabilizar_salida);
+
 		while (1) {
+			// Se crea un paciente aleatorio
 			char* paciente = generar_nombre_paciente();
 
 			sleep(tiempo_aleatorio(1, 10));
@@ -201,6 +245,7 @@ int main(int argv, char* argc[]) {
 
 			int resultado;
 			do {
+				// Mandamos el nombre del paciente a "Exploración".
 				resultado = mq_send(cola_recep_explor, paciente, strlen(paciente) + 1, 0);
 
 				if(resultado == -1) {
@@ -212,26 +257,4 @@ int main(int argv, char* argc[]) {
 	}
 
 	return 0;
-}
-
-#define NUM_NOMBRES 50
-char* generar_nombre_paciente() {
-	// Nota: Este array de nombres ha sido
-	// generada con ChatGPT.
-	// https://chatgpt.com/share/6759e9f5-8740-800e-a3a8-467ffc634fac
-
-	static char nombres[NUM_NOMBRES][128] = {
-    	"Alejandro", "Beatriz", "Carlos", "Diana", "Eduardo",
-    	"Fernanda", "Gabriel", "Héctor", "Isabel", "Javier",
-    	"Karla", "Luis", "María", "Natalia", "Oscar",
-    	"Patricia", "Quirino", "Rafael", "Sofía", "Tomás",
-    	"Úrsula", "Vicente", "Walter", "Ximena", "Yolanda",
-    	"Zaira", "Andrea", "Braulio", "Camila", "Daniel",
-    	"Esteban", "Francisca", "Guillermo", "Hilda", "Iván",
-    	"José", "Laura", "Martín", "Nicolás", "Olga",
-    	"Paola", "Ramón", "Salvador", "Teresa", "Ulises",
-    	"Valeria", "Wilfredo", "Xiomara", "Yahir", "Zoila"
-	};
-
-	return nombres[rand() % NUM_NOMBRES];
 }
--- old/recepcion.c
+++
@@ -1,8 +0,0 @@
-#include "recepcion.h"
-#include <stdio.h>
-
-void inicializar_Recepcion(Recepcion *recepcion, struct Cola *cola){
-    recepcion->cola_Pacientes = cola;//Registrar un paciente es basicamente encolarlo
-    recepcion->pacientes_Alta = 0;
-    }
-
